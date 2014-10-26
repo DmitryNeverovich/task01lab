fdc8b03d66394d003abb3dbd20ece1003b34035a
Dima <Dima@Dima-PC>
2014-10-26 23:01:10 +0300

    add comment

--- src/main/java/com/epam/testapp/service/ServiceException.java
+++ src/main/java/com/epam/testapp/service/ServiceException.java
@@ -1,4 +1,3 @@
-
 package com.epam.testapp.service;
 
 import com.epam.testapp.database.*;
@@ -6,28 +5,41 @@ import com.epam.testapp.exception.ProjectException;
 
 /**
  * Exception that provides information about error in Service
- * 
+ *
  */
-public class ServiceException extends ProjectException{
-        private static final long serialVersionID = 1L;
+public class ServiceException extends ProjectException {
+
+    private static final long serialVersionID = 1L;
 
     public ServiceException() {
         super();
-    }    
+    }
+
     /**
-     *  DAOException using given message and given hidden exception
-     * @param msg The message explaining the reason for the exception 
+     * ServiceException using given message and given hidden exception
+     *
+     * @param msg The message explaining the reason for the exception
      */
     public ServiceException(String msg) {
         super(msg);
     }
 
     /**
-     *  DAOException using given message
-     * @param msg The message explaining the reason for the exception 
+     * ServiceException using given message
+     *
+     * @param msg The message explaining the reason for the exception
      * @param e exception that will be wrapped
      */
     public ServiceException(String msg, Exception e) {
-        super(msg,e);
+        super(msg, e);
+    }
+
+    /**
+     * ServiceException using given hidden exception.
+     *
+     * @param e throwable that will be wrapped
+     */
+    public ServiceException(Throwable exception) {
+        super(exception);
     }
 }
